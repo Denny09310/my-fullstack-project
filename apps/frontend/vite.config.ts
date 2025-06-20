64b7cf2c30212e7608ded25b1a90fcf281c9246a
Koja sig. Dennis <DKOJA@ceia-spa.com>
2025-06-20 11:41:58 +0200

    fix: prefixed with VITE_ environment variable

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -8,14 +8,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
     plugins: [react(), generouted(), tailwindcss()],
     server: {
       proxy: {
         "^/api": {
-          target: process.env.API_TARGET,
+          target: process.env.VITE_API_TARGET,
           secure: mode === "development",
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
